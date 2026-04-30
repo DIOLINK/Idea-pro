282ebaa68dce87a960ff04ec1bb35c9d6964404a
Roger BC <roger@chaindots.com>
2026-04-30 01:01:43 -0300

    feat: Mejorar el diseño del componente InfoHeader con ajustes en el espaciado y escalado

--- src/components/molecules/InfoHeader.js
+++ src/components/molecules/InfoHeader.js
@@ -1,7 +1,15 @@
 import InfoRow from './InfoRow';
-export default function InfoHeader({ alumno, profesor, titulo, fecha_revision }) {
+export default function InfoHeader({
+  alumno,
+  profesor,
+  titulo,
+  fecha_revision,
+}) {
   return (
-    <div className="mt-2 flex justify-center space-x-8">
+    <div
+      className="mt-6 flex justify-center space-x-8 scale-105"
+      style={{ transform: 'scale(1.05)', marginTop: '2.5rem' }}
+    >
       <InfoRow label="Alumno:" value={alumno} />
       <InfoRow label="Profesor:" value={profesor} />
       <InfoRow label="Título:" value={titulo} />
