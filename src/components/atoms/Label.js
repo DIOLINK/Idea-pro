de006274a1a1fbb63ad211faef35f87be52a9f44
Roger BC <roger@chaindots.com>
2026-04-30 00:35:35 -0300

    feat: Actualizar estilos del componente Label para usar colores de tema claro y oscuro

--- src/components/atoms/Label.js
+++ src/components/atoms/Label.js
@@ -1,3 +1,3 @@
 export default function Label({ children }) {
-  return <span className="font-bold text-gray-700 mr-1">{children}</span>;
+  return <span className="font-bold text-primary-light dark:text-primary-dark mr-1">{children}</span>;
 }
